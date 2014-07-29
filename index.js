7d2e0b61922519b523348f459c2318caac908f1e
Matthew Vasseur <mattvasseur@gmail.com>
2014-07-28 21:09:47 -0400

    play, pause, next functionality

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-jQuery.ajaxSettings.traditional = true;
-
 // style functions
 function goGame() {
   $("#game").show();
   $("#home").hide();
-  fetchSongs("");
 }
 
 function goHome() {
@@ -16,36 +13,44 @@ function goInfo() {
   $("#info").toggle();
 }
 
-var curSong = null;
-var allSongs;
+function initUI() {
+  // initialize show/hide
+  $("#game").hide();
+  $("#home").show();
+  $("#info").hide();
+}
+
+jQuery.ajaxSettings.traditional = true; //idk what this does
+
+var MAX_SONGS = 10; // maximum number of songs
+var curSong = null; // current song being played
+var allSongs; // list of all 10 songs for the game
+var index; // index number for song list
 
 
-// fetchSongs(u): fetch a list of songs from specified url
-function fetchSongs(u) {
-  var url; //url from which to fetch songs
+// fetchSongs(url, args): fetch a list of songs from specified url with args
+function fetchSongs(url, args) {
 
-  if (u =="") {
-    url = 'songs.js';
-  } else {
-    url = u;
-  }
-
-  console.log("Attempting to fetch songs.");
-  $.getJSON(url, {},
+  console.log("Attempting to fetch songs ...");
+  $.getJSON(url, args,
             function(data) {
               allSongs = data['songs'];
-              console.log("SUCCESS!\n");
+              console.log("... SUCCESS!\n");
             },
             function() {
-              console.log("Trouble getting the song list.\n");
+              console.log("... Trouble getting the song list.\n");
               error("Trouble getting the song list.");
             }
            );
 }
 
+// fetchNextSong(): fetch the next song in the playlist
+function fetchNextSong() {
+  playSong(allSongs[++index]);
+}
+
 // getRdioID(song): get the Rdio ID for a given song
 function getRdioID(song) {
-  console.log(song);
   console.log("getting id from " + song.title + ".");
 
   var rawID = song.tracks[0].foreign_id; // get raw id form
@@ -56,25 +61,29 @@ function getRdioID(song) {
   return id;
 }
 
-//playMusic(): beings playing music
-function startGame() {
-  var songID;
-  songID = getRdioID(allSongs[0]);
+// playSong(): being playing the song
+function playSong(song) {
+  var songID = getRdioID(song);
+  curSong = song;
+
   $('#api').rdio().play(songID);
 }
 
+//playMusic(): beings playing music
+function playMusic() {
+  index = 0;
+  playSong(allSongs[index]);
+}
+
 $(document).ready(function(){
-
-
-  // initialize css
-  $("#game").hide();
-  $("#home").show();
-  $("#info").hide();
+  initUI();
+  fetchSongs("./songs.js", {}); //default song list
 
   $('#api').bind('ready.rdio', function() {
     playMusic();
   });
 
+  /*
   $('#api').bind('playingTrackChanged.rdio', function(e, playingTrack, sourcePosition) {
     if (playingTrack) {
       duration = playingTrack.duration;
@@ -88,23 +97,32 @@ $(document).ready(function(){
   $('#api').bind('positionChanged.rdio', function(e, position) {
     $('#position').css('width', Math.floor(100*position/duration)+'%');
   });
+  */
 
   $('#api').bind('playStateChanged.rdio', function(e, playState) {
     if (playState == 0) { // paused
-      $('#play').show();
-      $('#pause').hide();
+      $('#play').removeClass('btn-primary');
+      $('#pause').removeClass('btn-info');
+
+      $('#play').addClass('btn-info');
+      $('#pause').addClass('btn-primary');
     } else {
-      $('#play').hide();
-      $('#pause').show();
+      $('#play').removeClass('btn-info');
+      $('#pause').removeClass('btn-primary');
+
+      $('#play').addClass('btn-primary');
+      $('#pause').addClass('btn-info');
     }
   });
 
-  // this is a valid playback token for localhost.
-  // but you should go get your own for your own domain.
+  // add playback token from token.js
   $('#api').rdio(playback_token);
 
-  $('#previous').click(function() { $('#api').rdio().previous(); });
-  $('#play').click(function() { $('#api').rdio().play(); });
-  $('#pause').click(function() { $('#api').rdio().pause(); });
-  $('#next').click(function() { $('#api').rdio().next(); });
+  // player controls
+  $('#play').click(function() { $('#api').rdio().play();}); // play
+  $('#pause').click(function() { $('#api').rdio().pause(); }); //pause
+  // next song
+  $('#next').click(function() {
+    fetchNextSong();
+  });
 });
